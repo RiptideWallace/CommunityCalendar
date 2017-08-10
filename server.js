e94c1be4a5ded229dd733f73febbc716761c97ef
RiptideWallace <brysonc1987@gmail.com>
2017-08-10 19:51:23 +0000

    initial setup and GET routes added

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
-const express = require("express");
-const PORT = 3000;
-const app = express();
+const express       = require("express");
+const PORT          = 3000;
+const app           = express();
+const jquery        = require("jquery");
+const pg            = require("pg");
+const knex          = require("knex");
+const ejs           = require("ejs");
+const bcrypt        = require("bcrypt");
+const cookieSession = require('cookie-session');
+const bodyParser    = require("body-parser");
+const sass          = require("node-sass-middleware");
+const bootstrap     = require("bootstrap");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true}));\
+app.use(express.static("public"));
+
+//Home Page
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+//Search Page
+app.get("/search", (req, res) => {
+  res.render("search")
+});
+
+app.get("/event", (req, res) => {
+  res.render("event")
+});
 
 app.listen(PORT, () =>{
   console.log("Listening in on Port " + PORT)
