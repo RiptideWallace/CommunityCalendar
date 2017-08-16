e73d7c00010bdac4d5e8734518ac2a3a871614fc
RiptideWallace <brysonc1987@gmail.com>
2017-08-16 19:52:07 +0000

    place data inserted into seed file, drop down links display places associated with that region

--- db/seeds/6_regions.js
+++ db/seeds/6_regions.js
@@ -7,7 +7,15 @@ exports.seed = function(knex, Promise) {
         knex('regions').insert({id: 2, name: 'Oak Bay', slug: 'oak-bay'}),
         knex('regions').insert({id: 3, name: 'Esquimalt', slug: 'esquimalt'}),
         knex('regions').insert({id: 4, name: 'Saanich', slug: 'saanich'}),
-        knex('regions').insert({id: 5, name: 'Langford', slug: 'langford'})
+        knex('regions').insert({id: 5, name: 'Langford', slug: 'langford'}),
+        knex('regions').insert({id: 6, name: 'Metchosin', slug: 'metchosin'}),
+        knex('regions').insert({id: 7, name: 'North Saanich', slug: 'north-saanich'}),
+        knex('regions').insert({id: 8, name: 'Colwood', slug: 'colwood'}),
+        knex('regions').insert({id: 9, name: 'Highlands', slug: 'highlands'}),
+        knex('regions').insert({id: 10, name: 'Sidney', slug: 'sidney'}),
+        knex('regions').insert({id: 11, name: 'Sooke', slug: 'sooke'}),
+        knex('regions').insert({id: 12, name: 'Central Saanich', slug: 'central-saanich'}),
+        knex('regions').insert({id: 13, name: 'View Royal', slug: 'view-royal'}),
       ]);
     });
 };
