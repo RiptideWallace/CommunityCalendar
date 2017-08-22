cc27a3b313addaedf766e54c4fccc8119a58b87f
Tyler Krys <tylerkrys@gmail.com>
2017-08-22 02:38:39 +0000

    Removes unused index.js jQuery script

--- public/scripts/index.js
+++
@@ -1,9 +0,0 @@
-// Stop propagation on mobile touchscreens to allow dropdown links to work
-$('.dropdown-toggle').click(function(e) {
-  e.preventDefault();
-  setTimeout($.proxy(function() {
-    if ('ontouchstart' in document.documentElement) {
-      $(this).siblings('.dropdown-backdrop').off().remove();
-    }
-  }, this), 0);
-});
