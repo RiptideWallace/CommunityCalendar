bf5c25404c90177402e980bc81a08d802abf3abf
Tyler Krys <tylerkrys@gmail.com>
2017-08-22 01:38:55 +0000

    Refactors jQuery targeting code to allow dropdown links to function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,9 @@
 // Stop propagation on mobile touchscreens to allow dropdown links to work
-$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+$('.dropdown-toggle').click(function(e) {
+  e.preventDefault();
+  setTimeout($.proxy(function() {
+    if ('ontouchstart' in document.documentElement) {
+      $(this).siblings('.dropdown-backdrop').off().remove();
+    }
+  }, this), 0);
+});
