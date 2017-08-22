399da1e4b2e26ca2f25fd0010f0b2d256099540d
Tyler Krys <tylerkrys@gmail.com>
2017-08-22 23:21:11 +0000

    Refactors server.js with routes broken into several imported files in routes/

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 require('dotenv').config();
 
-const express          = require("express");
-const PORT             = process.env.PORT || 3000;
-const ENV              = process.env.NODE_ENV || "development";
-const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
-const app              = express();
-const helmet           = require('helmet');
-const pg               = require("pg");
-const knexConfig       = require('./knexfile');
-const knex             = require('knex')(knexConfig[ENV]);
-const knexLogger       = require('knex-logger');
-const ejs              = require('ejs');
-const bcrypt           = require('bcrypt');
-const cookieSession    = require('cookie-session');
-const bodyParser       = require('body-parser');
-const sass             = require('node-sass-middleware');
-const basicAuth        = require("basic-auth");
-const secureRoute      = require("secure-route");
-const slug             = require('slug')
+const express           = require("express");
+const PORT              = process.env.PORT || 3000;
+const ENV               = process.env.NODE_ENV || "development";
+const googleMapsApiKey  = process.env.GOOGLE_MAPS_API_KEY;
+const app               = express();
+const helmet            = require('helmet');
+const pg                = require("pg");
+const knexConfig        = require('./knexfile');
+const db                = require('./db');
+const ejs               = require('ejs');
+const cookieSession     = require('cookie-session');
+const bodyParser        = require('body-parser');
+const sass              = require('node-sass-middleware');
+const basicAuth         = require("basic-auth");
+const secureRoute       = require("secure-route");
+const directoryRoutes   = require('./routes/directory');
+const eventCreateRoutes = require('./routes/create-event');
+const eventSaveRoutes   = require('./routes/event');
+const placeRoutes       = require('./routes/place');
+const priceRoutes       = require('./routes/price');
+const userRoutes        = require('./routes/user');
 
-slug.defaults.mode     = 'rfc3986';
+db.init(app, knexConfig[ENV]);
+const knex = db.handle();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
-app.use(knexLogger(knex));
+
+// Use Helmet for secure HTTP headers
 app.use(helmet());
 
 app.use(cookieSession({
@@ -33,8 +38,9 @@ app.use(cookieSession({
 
 app.use((req, res, next) => {
   res.locals.userId = req.session.userId;
+  res.locals.apiKey = googleMapsApiKey;
   next();
-})
+});
 
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -43,45 +49,18 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 
-function randomString() {
-  let output = "";
-  let characters = "abcdefghijklmnopqrstuvwxyz1234567890";
-  for (let i = 0; i < 6; i++) {
-    output += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return output;
-}
-
-function getPriceRangeString(priceParameter) {
-  let priceString = "";
-  switch (parseInt(priceParameter)) {
-    case 0:
-      priceString = "Free Events";
-      break;
-    case 1:
-      priceString = "Events Less Than $10";
-      break;
-    case 2:
-      priceString = "Events $10 and Up";
-      break;
-    case 3:
-      priceString = "Events $100 and Up";
-      break;
-  }
-  return priceString;
-}
-
 // For production (Heroku) http:// requests, redirect to https://
 if (app.get('env') === 'production') {
   app.use((req, res, next) => {
-    if (req.header('X-Forwarded-Proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
-      next()
-  })
+    if (req.header('X-Forwarded-Proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
 }
 
-//Home Page
+// Route to home page (GET)
 app.get("/", (req, res) => {
   knex('regions')
     .select([
@@ -109,404 +88,19 @@ app.get("/", (req, res) => {
     });
 });
 
-//Register Page (GET)
-app.get("/register", (req, res) => {
-  res.render("register")
-});
+// Imported routes from ./routes directory
+app.use('/BC', directoryRoutes);
+app.use('/create-event', eventCreateRoutes);
+app.use('/event', eventSaveRoutes);
+app.use('/user', userRoutes);
+app.use('/place', placeRoutes);
+app.use('/price', priceRoutes);
 
-//Register Page (POST)
-app.post("/register", (req, res) => {
-  if (req.body.password === req.body.password_confirmation) {
-  knex('users')
-    .insert({
-      name: req.body.username,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10)
-    })
-    .then(() => {
-      return knex
-        .select("*")
-        .from('users')
-        .where({email: req.body.email});
-      })
-        .then((results) => {
-          req.session.userId = results[0].id;
-          res.redirect("/")
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(404).send("Could not complete registration");
-        })
-  } else {
-    res.status(404).send("Password and confirmation don't match");
-  }
-});
-
-//Login Page (GET)
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-//Create Event Page (GET)
-app.get("/create-event", (req, res, next) => {
-  knex('places')
-    .select([
-      'places.id as id',
-      'places.name as place_name'
-      ])
-      .orderBy('id')
-      .then((results) => {
-        console.log(results);
-        let templateVars = {
-          place: results,
-        }
-        res.render("create-event", templateVars);
-  });
-});
-
-//Create Event (POST)
-app.post("/create-event", (req, res) => {
-  knex('activities')
-    .insert ({
-      place_id: req.body.placeID,
-      name: req.body.event,
-      description: req.body.description,
-      start_date: req.body.start_date,
-      end_date: req.body.end_date,
-      price_range: req.body.price_range,
-      source: req.body.source,
-      slug: (slug(req.body.event)) + "-" + randomString()
-    })
-    .then((results) => {
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send("Event Not Saved");
-    })
-    res.redirect("/");
-})
-
-//Login Page (POST)
-app.post("/login", (req, res) => {
-  knex
-    .select("*")
-    .from('users')
-    .where({email: req.body.email})
-    .then((results) => {
-      if (results.length === 0) {
-        res.status(404).send("Login failed");
-        return;
-      }
-      if (!bcrypt.compareSync(req.body.password, results[0].password)) {
-        res.status(404).send("Login failed");
-        return;
-      }
-      req.session.userId = results[0].id;
-      res.redirect("/");
-    });
-});
-
-//Logout (POST)
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/");
-});
-
-// Route to show a user's profile page
-app.get("/users/:id/show", (req, res) => {
-  knex('users')
-  .where({id: req.params.id})
-  .then((userResults) => {
-    if (userResults.length === 0) {
-      res.status(404).send("This user does not exist");
-      return;
-    }
-    knex
-      .select([
-        '*',
-        'activities.name as activity_name',
-        'activities.start_date as activity_start_date',
-        'activities.end_date as activity_end_date',
-        'activities.source as activity_source',
-        'activities.slug as activity_slug',
-        'regions.slug as region_slug',
-        'regions.name as region_name',
-        'places.slug as place_slug',
-        'places.name as place_name',
-        'places.street_address as street_address'
-      ])
-    .from("saved-events")
-    .leftJoin('activities', 'saved-events.activity_id', 'activities.id')
-    .leftJoin('places', 'activities.place_id', 'places.id')
-    .leftJoin('regions', 'places.region_id', 'regions.id')
-    .where({'saved-events.user_id': req.params.id})
-    .then((savedResults) => {
-      knex
-        .select([
-          '*',
-          'places.name as place_name',
-          'places.slug as place_slug',
-          'regions.name as region_name',
-          'regions.slug as region_slug',
-        ])
-      .from("favourited-places")
-      .join('places', 'favourited-places.place_id', 'places.id')
-      .join('regions', 'places.region_id', 'regions.id')
-      .where({'favourited-places.user_id': req.params.id})
-      .then((favedResults) => {
-        let templateVars = {
-          user: userResults[0],
-          savedEvents: savedResults,
-          favedPlaces: favedResults
-        };
-        res.render("user", templateVars);
-      });
-    });
-  });
-});
-
-// Route to an event's page with URLs in slug form
-app.get('/BC/:region/:place/:activity', function(req, res, next) {
-  knex('activities')
-    .select([
-      'activities.id as id',
-      'activities.name as name',
-      'activities.start_date',
-      'activities.end_date',
-      'activities.price_range',
-      'activities.source',
-      'activities.description',
-      'activities.slug as activity_slug',
-      'places.id as place_id',
-      'places.name as place_name',
-      'places.slug as place_slug',
-      'places.abbreviation',
-      'places.street_address',
-      'places.latitude as latitude',
-      'places.longitude as longitude',
-      'regions.id as region_id',
-      'regions.slug as region_slug',
-      'regions.name as region_name'
-    ])
-    .join('places', 'places.id', '=', 'activities.place_id')
-    .join('regions', 'regions.id', '=', 'places.region_id')
-    .where({
-      'activities.slug': req.params.activity,
-      'places.slug': req.params.place,
-      'regions.slug': req.params.region
-    })
-    .then((results) => {
-      console.log(results);
-      if (results.length === 0) {
-        res.status(404).send("This activity does not exist");
-        return;
-      }
-      let templateVars = {
-        activity: results[0],
-        apiKey: googleMapsApiKey
-      }
-      res.render("event", templateVars);
-    });
-});
-
-// Route for when a search is conducted on a place
-app.get('/BC/:region/:place', function(req, res) {
-  knex.select([
-    'places.id as place_id',
-    'places.name as place_name',
-    'places.slug as place_slug',
-    'places.street_address as street_address',
-    'places.latitude',
-    'places.longitude',
-    'regions.id as region_id',
-    'regions.slug as region_slug',
-    'regions.name as region_name',
-    knex.raw(`COALESCE(JSON_AGG(activities.*) FILTER (WHERE activities.id IS NOT NULL), '[]') AS activities`)
-    ])
-    .from('regions')
-    .leftJoin('places', 'regions.id', 'places.region_id')
-    .leftJoin('activities', 'places.id', 'activities.place_id')
-    .where({
-      'places.slug': req.params.place,
-      'regions.slug': req.params.region
-    })
-    .groupBy(
-      'places.id',
-      'places.name',
-      'places.slug',
-      'places.street_address',
-      'places.latitude',
-      'places.longitude',
-      'regions.id',
-      'regions.slug',
-      'regions.name'
-    )
-    .then((results) => {
-      console.log(results);
-      let templateVars = {
-        place: results[0],
-        activities: results[0].activities,
-        apiKey: googleMapsApiKey
-      }
-      res.render("event-search", templateVars);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).send("No such place");
-    });
-});
-
-//Route for when a search is conducted on a region
-app.get('/BC/:region', (req, res) => {
-   knex('places')
-    .select([
-      'places.id as id',
-      'places.name as name',
-      'places.abbreviation',
-      'places.street_address as street_address',
-      'places.slug as place_slug',
-      'regions.id as region_id',
-      'regions.name as region_name',
-      'regions.slug as region_slug'
-    ])
-    .join('regions', 'regions.id', '=', 'places.region_id')
-    .where({'regions.slug': req.params.region})
-    .then((results) => {
-      console.log(results);
-      if (results.length === 0) {
-        res.status(404).send("This place does not exist");
-        return;
-      }
-      let templateVars = {
-        place: results,
-      }
-      res.render("place-search", templateVars);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).send("Event not saved");
-    });
-});
-
-//Route for when a search is conducted by price range
-app.get('/price/:pricerange', (req, res) => {
-  knex('activities')
-   .select([
-      'activities.id as id',
-      'activities.name as name',
-      'activities.start_date',
-      'activities.end_date',
-      'activities.price_range',
-      'activities.source',
-      'activities.description',
-      'activities.slug as activity_slug',
-      'places.id as place_id',
-      'places.name as place_name',
-      'places.abbreviation',
-      'places.street_address',
-      'places.slug as place_slug',
-      'regions.id as region_id',
-      'regions.name as region_name',
-      'regions.slug as region_slug'
-    ])
-    .join('places', 'places.id', '=', 'activities.place_id')
-    .join('regions', 'regions.id', '=', 'places.region_id')
-    .where({
-      'activities.price_range': parseInt(req.params.pricerange) || 0,
-    })
-    .then((results) => {
-      let priceRangeString = getPriceRangeString(req.params.pricerange)
-      let templateVars = {
-        activity: results,
-        price: priceRangeString
-      }
-      res.render("price-search", templateVars);
-    });
-});
-
-// Route for when an event is saved by a user
-app.post('/event/saved/:activityId/:userId', (req, res) => {
-  knex('saved-events')
-    .select('*')
-    .where({
-      'activity_id': req.params.activityId,
-      'user_id': req.params.userId
-    })
-    .then((searchResults) => {
-      if (searchResults.length >= 1) {
-        res.redirect(`/users/${req.params.userId}/show`)
-      } else {
-        knex('saved-events')
-          .insert({activity_id: req.params.activityId, user_id: req.params.userId})
-          .then((results) => {
-            res.redirect(`/users/${req.params.userId}/show`)
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(400).send("Event not saved");
-          })
-      }
-    })
-})
-
-// Route for when a saved event is deleted by a user
-app.post('/event/delete/:activityId/:userId', (req, res) => {
-  knex('saved-events')
-    .where({activity_id: req.params.activityId, user_id: req.params.userId})
-    .del()
-    .then((results) => {
-      res.redirect(`/users/${req.params.userId}/show`)
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send("Event could not be deleted");
-    })
-})
-
-// Route for when a place is favourited by a user
-app.post('/place/saved/:placeId/:userId', (req, res) => {
-  knex('favourited-places')
-    .select('*')
-    .where({
-      'place_id': req.params.placeId,
-      'user_id': req.params.userId
-    })
-    .then((searchResults) => {
-      if (searchResults.length >= 1) {
-        res.redirect(`/users/${req.params.userId}/show`)
-      } else {
-        knex('favourited-places')
-          .insert({place_id: req.params.placeId, user_id: req.params.userId})
-          .then((results) => {
-            res.redirect(`/users/${req.params.userId}/show`)
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(400).send("Place not favourited")
-          })
-      }
-    })
-})
-
-// Route for when a saved event is deleted by a user
-app.post('/place/delete/:placeId/:userId', (req, res) => {
-  knex('favourited-places')
-    .where({place_id: req.params.placeId, user_id: req.params.userId})
-    .del()
-    .then((results) => {
-      res.redirect(`/users/${req.params.userId}/show`)
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send("Place could not be un-favourited");
-    })
-})
-
-// 404 Catch-all
+// Route for 404 catch-all
 app.get('*', function(req, res){
   res.status(404).render("404");
 });
 
-app.listen(PORT, () =>{
-  console.log("Listening on port " + PORT)
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
 });
